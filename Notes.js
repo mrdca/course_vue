3b118d8140188adaf46e95d70141fbdc2d8df137
David Collazo <dcalvarez1994@gmail.com>
2018-04-12 20:06:40 -0500

    Vue course

---
+++ Notes.js
@@ -0,0 +1,9 @@
+// {{}} this is called Interpolation or String Interpolation
+
+//to bind data, use: v-bind:prop="element" -> example: <a v-bind:href="link">
+
+//to render only one time, use: v-once
+
+//to get a raw html element, use v-html:
+// element: '<a href="http://google.com">Google</a>'
+// <p v-html="element">
